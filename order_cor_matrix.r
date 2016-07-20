df27b8e15fec9f58e6d6747292dda99fb7c4d2b0
mertbozfakioglu <mertbozfakioglu@yahoo.com>
2016-07-20 15:36:09 -0500

    added p values

--- order_cor_matrix.r
+++ order_cor_matrix.r
@@ -1,13 +1,24 @@
-order_corr_matrix <- function(corr_matrix)
+order_corr_matrix <- function(corr_matrix,p_matrix)
 {
-	corr <- read.table(file = corr_matrix, header = T, sep = "\t", row.names = 1)
-	# replace all NA with 0 so absolute value function works
-
-	a <- apply(corr, 1, function(row) {
-			   rowWithName <- paste(names(row), row, sep=":")
-			   rowWithName[order(abs(as.numeric(row)), decreasing = T)]
-				})
-	write.table(a, file = paste0(corr_matrix, ".ordered"), sep = "\t", row.names = F)
+  corr <<- read.table(file = corr_matrix, header = T, sep = "\t",row.names = 1)
+  # replace all NA with 0 so absolute value function works
+  p <<- read.table(file = p_matrix, header = T, sep = "\t", row.names = 1)
+  for (y in 1:length(corr)){
+    for (x in 1:length(corr[,1])){
+      corr [x,y] <- paste(corr[x,y],p[x,y],sep = ":")
+    }
+  }
+  corr <<- corr
+  
+  a <<- apply(corr, 1, function(row) {
+    #row <- gsub( " .*$", "", row )
+    rowWithName <- paste(names(row), row, sep=":")
+    #print(row)
+    rowWithName[order(abs(as.numeric(gsub( ":.*$", "", row ))), decreasing = T)]
+    })
+  
+  
+  write.table(a, file = paste0(corr_matrix, ".ordered"), sep = "\t", row.names = F)
 }
 
 order_all_matrices <- function()
@@ -17,8 +28,8 @@ order_all_matrices <- function()
 		if(file.exists(file.path(dir, "cor_summary.txt")))
 		{
 			print(dir)
-			order_corr_matrix(file.path(dir,"cor_summary_pearson.txt"))
-			order_corr_matrix(file.path(dir,"cor_summary_spearman.txt"))
+			order_corr_matrix(file.path(dir,"cor_summary_pearson.txt"),p_matrix = "cor_summary_pearson_test.txt")
+			order_corr_matrix(file.path(dir,"cor_summary_spearman.txt"),p_matrix = "cor_summary_spearman_test.txt")
 		}
 	}
 }
