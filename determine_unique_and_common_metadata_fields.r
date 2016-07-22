5ab188fe22ea515330bc06b12880b6dc162cd33f
mertbozfakioglu <mertbozfakioglu@yahoo.com>
2016-07-22 14:06:39 -0500

    Add files via upload

---
+++ determine_unique_and_common_metadata_fields.r
@@ -0,0 +1,16 @@
+#determine_unique_and_common_metadata_fields
+create_frequency_table_for_significant_metadata_fields <- function(sig_meta_fields_file)
+{
+  fields <- c()
+  for(dir in list.dirs())
+  {
+    if(file.exists(file.path(dir, sig_meta_fields_file)))
+    {
+      print(dir)
+      sig_file <- unlist(read.table(file.path(dir,sig_meta_fields_file),stringsAsFactors = F))
+      fields <- c(fields,sig_file)
+    }
+  }
+  tab <- (table(fields))
+  write.table(tab,"significant_metadata_frequencies")
+}
