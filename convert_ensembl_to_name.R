d9e0f2644265a3a4e76a533f797bea0918ec40fd
Ronald Shi <RonaldHShi@uchicago.edu>
2016-08-08 10:54:31 -0500

    convert_ensembl_to_name.R

---
+++ convert_ensembl_to_name.R
@@ -0,0 +1,11 @@
+library(biomaRt)
+
+ConvertEnsemblToName <- function(ensembl.genes) {
+  mart <- useDataset("hsapiens_gene_ensembl", useMart("ensembl"))
+  gene.names <- getBM(
+    filters= "ensembl_gene_id", 
+    attributes= c("ensembl_gene_id", "external_gene_name", "description"),
+    values= ensembl.genes,
+    mart= mart)
+  return(gene.names)
+}
