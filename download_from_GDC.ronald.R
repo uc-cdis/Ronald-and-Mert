270a1d493e6e8ceecde264e805730a5fea0eae7f
Ronald Shi <RonaldHShi@uchicago.edu>
2016-08-24 14:11:08 -0500

    finishing

--- download_from_GDC.ronald.R
+++ download_from_GDC.ronald.R
@@ -16,60 +16,84 @@ CompareNormalCancerPipeline <- function(projects) {
     }
     # loop through vec_of_projects and download all HTSeq counts files
     for(i in vec_of_projects) {
-      if (size_lim) {
-        download_normal_from_GDC(i, size_lim = size_lim)
-      } else {
-        download_normal_from_GDC(i)
-      }
+      download_normal_from_GDC(i)
     }
   } else {
     # else if user inputed a vector of project_ids
     for(i in projects) {
-      if (size_lim) {
-        download_normal_from_GDC(i, size_lim = size_lim)
-      } else {
-        download_normal_from_GDC(i)
-      }
+      download_normal_from_GDC(i)
     }
   }
+  system("gunzip *.gz")
   system("ls | grep .counts$ > normal_filenames")
   vec.normal.filenames <- scan("normal_filenames", what = "character", sep = "\n")
+  browser()
   for (f in vec.normal.filenames) {
-    my_call <- paste0("https://gdc-api.nci.nih.gov/files?fields=file_id&size=99999&pretty=true&filters=%7B%22op%22%3A%22and%22%2C%22content%22%3A%5B%7B%22op%22%3A%22in%22%2C%22content%22%3A%7B%22field%22%3A%22cases.samples.sample_type%22%2C%22value%22%3A%5B%22Blood+Derived+Normal%22%2C%22Solid+Tissue+Normal%22%2C%22Bone+Marrow+Normal%22%2C%22Fibroblasts+from+Bone+Marrow+Normal%22%2C%22Buccal+Cell+Normal%22%5D%7D%7D%2C%7B%22op%22%3A%22in%22%2C%22content%22%3A%7B%22field%22%3A%22analysis.workflow_type%22%2C%22value%22%3A%5B%22HTSeq+-+Counts%22%5D%7D%7D%2C%7B%22op%22%3A%22in%22%2C%22content%22%3A%7B%22field%22%3A%22files.data_format%22%2C%22value%22%3A%5B%22TXT%22%5D%7D%7D%2C%7B%22op%22%3A%22%3D%22%2C%22content%22%3A%7B%22field%22%3A%22cases.project.project_id%22%2C%22value%22%3A%5B%22", p, "%22%5D%7D%7D%5D%7D")
+    file.name <- strsplit(f, "\\.")[[1]][1]
+    my_call <- paste0("https://gdc-api.nci.nih.gov/cases?fields=case_id&size=99999&pretty=true&filters=%7b%0d%0a+++%22op%22+%3a+%22%3d%22+%2c%0d%0a+++%22content%22+%3a+%7b%0d%0a+++++++%22field%22+%3a+%22files.file_id%22+%2c%0d%0a+++++++%22value%22+%3a+%5b+%22", f, "%22+%5d%0d%0a+++%7d%0d%0a%7d")
     my_call.json <- fromJSON(getURL(my_call))
-    UUID.list <- unlist(my_call.json$data$hits)
-    for(j in UUID.list) {
-      print(paste0(j, ": ", j))
-      system(paste0("curl --remote-name --remote-header-name 'https://gdc-api.nci.nih.gov/data/", 
-                    j, "'"))
-    }
-    # merges abundance data together
-  to_log("Merging data")
-  GDC_raw_count_merge(id_list="counts_files")
-  to_log("Merge completed")
-
-    # gets UUIDs of data merged together
-    to_log("Exporting UUID's")
-    export_listof_UUIDs(tsv = "counts_files.merged_data.txt")
-    to_log("Export Completed")
+    case.id <- unlist(my_call.json$data$hits)
+    my_call <- paste0("https://gdc-api.nci.nih.gov/files?fields=file_id&size=99999&pretty=true&filters=%7B%0D%0A%09%22op%22%3A%22and%22%2C%0D%0A%09%22content%22%3A%5B%7B%0D%0A%09%09%22op%22%3A%22in%22%2C%0D%0A%09%09%22content%22%3A%7B%0D%0A%09%09%09%22field%22%3A%22cases.samples.sample_type%22%2C%0D%0A%09%09%09%22value%22%3A%5B%22Primary+Tumor%22%5D%0D%0A%09%09%09%7D%0D%0A%09%09%7D%2C%7B%0D%0A%09%09%22op%22%3A%22in%22%2C%0D%0A%09%09%22content%22%3A%7B%0D%0A%09%09%09%22field%22%3A%22analysis.workflow_type%22%2C%0D%0A%09%09%09%22value%22%3A%5B%22HTSeq+-+Counts%22%5D%0D%0A%09%09%09%7D%0D%0A%09%09%7D%2C%7B%0D%0A%09%09%22op%22%3A%22in%22%2C%0D%0A%09%09%22content%22%3A%7B%0D%0A%09%09%09%22field%22%3A%22files.data_format%22%2C%0D%0A%09%09%09%22value%22%3A%5B%22TXT%22%5D%0D%0A%09%09%09%7D%0D%0A%09%09%7D%2C%7B%0D%0A%09%09%22op%22%3A%22%3D%22%2C%0D%0A%09++++%22content%22%3A%7B%0D%0A%09++++%09%22field%22%3A%22cases.case_id%22%2C%0D%0A%09++++%09%22value%22%3A%5B%22", case.id, "%22%5D%0D%0A%09++++%7D%0D%0A%09%7D%5D%0D%0A%7D")
+    my_call.json <- fromJSON(getURL(my_call))
+    matching.cancer.id <- unlist(my_call.json$data$hits)
+    system(paste0("curl --remote-name --remote-header-name 'https://gdc-api.nci.nih.gov/data/", 
+          matching.cancer.id, "'"))
   }
-  # gets metadata file
-  to_log("Getting Metadata")
-  get_GDC_metadata("counts_files.merged_data_file_UUIDs", my_rot = "yes")
 
-  # gets metadata_filename 
-  # (obsolete now but useful for interacting with old metadata filenames)
-  files <- list.files()[grep("GDC_METADATA.txt", list.files())]
-  details <- file.info(list.files()[grep("GDC_METADATA.txt", list.files())])
-  details <- details[with(details, order(as.POSIXct(mtime))), ]
-  metadata_filename <- rownames(details)[length(files)] 
-  print(metadata_filename)
-  to_log("Metadata Download Completed")
+  system("gunzip *.gz")
+  system("ls | grep .counts$ > all_filenames")
+
+  # merges abundance data together
+  GDC_raw_count_merge(id_list="all_filenames")
+
+  # gets UUIDs of data merged together
+  export_listof_UUIDs(tsv = "all_file_UUIDs")
+
+  # gets metadata file
+  get_GDC_metadata("all_file_UUIDs.merged_data_file_UUIDs", my_rot = "yes")
 
   # normalizing the data
-  to_log("Preprocessing")
-  preprocessing_tool(data_in = "counts_files.merged_data.txt", produce_boxplots = TRUE)
-  to_log("Preprocessing Completed")
+  preprocessing_tool(data_in = "all_filenames.merged_data.txt", produce_boxplots = TRUE)
+
+  # add normOrCancer col
+  metadata.table <- read.table(file="all_filenames.merged_data_file_UUIDs.GDC_METADATA.txt",row.names=1,header=TRUE,sep="\t",
+      colClasses = "character", check.names=FALSE,
+      comment.char = "",quote="",fill=TRUE,blank.lines.skip=FALSE)
+  normOrCancer <- c()
+  for (r in row.names(metadata.table)) {
+    if (r %in% vec.normal.filenames) {
+      normOrCancer <- c(normOrCancer, "Normal")
+    } else {
+      normOrCancer <- c(normOrCancer, "Cancer")
+    }
+  }
+  added.col.metadata.table <- cbind(metadata.table, normOrCancer)
+  write.table(added.col.metadata.table, file = "all_filenames.merged_data_file_UUIDs.GDC_METADATA.txt.addedcol", 
+      sep = "\t", quote = F, col.names = NA, row.names = T)
+
+  # generate PCoA
+  render_calculated_pcoa(PCoA_in= "all_filenames.merged_data.txt.DESeq_blind.PREPROCESSED.txt.euclidean.PCoA", 
+      metadata_table = "all_filenames.merged_data_file_UUIDs.GDC_METADATA.txt.addedcol", 
+      metadata_column_index = "normOrCancer")
+
+  # calc stats
+  calc_stats(data_table = "all_filenames.merged_data.txt.DESeq_blind.PREPROCESSED.txt", 
+      metadata_table = "all_filenames.merged_data_file_UUIDs.GDC_METADATA.txt.addedcol", 
+      metadata_column = "normOrCancer")
+
+  # take top 1% of genes
+  stats.table <- read.table(file = "counts_files.merged_data.txt.DESeq_blind.PREPROCESSED.txt.Kruskal-Wallis.normal.or.tumor.col.STATS_RESULTS.txt",
+      row.names = 1,header = TRUE,sep = "\t",
+      colClasses = "character", check.names = FALSE,comment.char = "", 
+      fill = TRUE, blank.lines.skip = FALSE)
+  filtered.stats.table <- stats.table[1:floor(nrow(stats.table)/100), 1:(ncol(stats.table) - 7)]
+  write.table(filtered.stats.table, file = "counts_files.merged_data.txt.DESeq_blind.PREPROCESSED.txt.Kruskal-Wallis.normal.or.tumor.col.STATS_REMOVED.filtered.txt", 
+      sep = "\t", quote = F, col.names = NA, row.names = T)
+
+  # generate heatmap
+  heatmap_dendrogram(file_in = "counts_files.merged_data.txt.DESeq_blind.PREPROCESSED.txt.Kruskal-Wallis.normal.or.tumor.col.STATS_REMOVED.filtered.txt", 
+      metadata_table = "all_filenames.merged_data_file_UUIDs.GDC_METADATA.txt.addedcol", 
+      metadata_column = "normOrCancer")
 }
 download_normal_from_GDC <- function(projects) {
 # download normal sample HTSeq Counts data for a given project
